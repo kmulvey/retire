62d3451581a32927794b9c4f59e9245aa4ac58b3
Kevin Mulvey <kmulvey@mediamath.com>
2017-07-21 20:35:32 -0600

    return var

--- main.go
+++ main.go
@@ -3,19 +3,21 @@ package main
 import "math"
 
 // Retire returns the amount needed in order to retire
-func Retire(currAge, retireAge, deathAge, currIncome int, inflation, rateOfReturn, rateOfSpend float64) float64 {
+func Retire(currAge, retireAge, deathAge, currIncome int, inflation, rateOfReturn, rateOfSpend float64) (total float64) {
 	var fVArr []float64
 
 	for i := retireAge; i <= deathAge; i++ {
 		fVArr = append(fVArr, futureValue(float64(currIncome)*rateOfSpend, inflation, i-currAge))
 	}
-	var total = 0.0
 	for _, num := range fVArr {
 		total += num
 	}
 	return total
 }
 
+func Ready() (result boolean) {
+
+}
 func futureValue(principal, inflation float64, years int) (result float64) {
 	return principal * math.Pow(1+inflation, float64(years))
 }
